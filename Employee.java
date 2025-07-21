02b34c0063e0ab960d5874de442d082fdace24bc
Thanushree R <rmntshree@gmail.com>
2025-07-21 11:05:59 +0530

    creating git java file

---
+++ Employee.java
@@ -0,0 +1,18 @@
+class Employee{
+    public static void main(String[] args){
+    int id=10;
+    long no=56576978;
+    float monthsalary=15000;
+    double yearsalary=200000;
+    char block='f';
+    String name="Ravi";
+    System.out.println("Welcome  employee");
+    System.out.println("kan"+3);
+    System.out.println(id);
+    System.out.println(no);
+    System.out.println(monthsalary);
+    System.out.println(yearsalary);
+    System.out.println(name);
+    System.out.println(block);
+    }
+}
