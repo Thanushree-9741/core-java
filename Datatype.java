d0d1a07d16f7574f2abc63e5ae4725d5f5492052
Thanushree R <rmntshree@gmail.com>
2025-07-21 11:03:36 +0530

    creating git java file

---
+++ Datatype.java
@@ -0,0 +1,10 @@
+class Datatype{
+    public static void main(String[] args){
+        System.out.println("Datatypes in java");
+        System.out.println("String");
+        System.out.println("Arrays");
+        System.out.println("Classes");
+        System.out.println("Interface");
+        System.out.println("Objects");
+    }
+}
