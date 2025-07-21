8162771fb696478adcc3ea30bd85aedae481f526
Thanushree R <rmntshree@gmail.com>
2025-07-21 11:11:04 +0530

    creating git java file

---
+++ Exception.java
@@ -0,0 +1,6 @@
+class Exception{
+    public static void main(String[] args){
+        System.out.println("types of exception handling");
+        System.out.println("1. try, 2.throw, 3.catch, 4.throw, 5.final");
+    }
+}
