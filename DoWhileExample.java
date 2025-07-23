f51961cb26412735b2fb9b4a4d72a2bf2e356c16
Thanushree R <rmntshree@gmail.com>
2025-07-23 19:01:48 +0530

    creating git java file

---
+++ DoWhileExample.java
@@ -0,0 +1,10 @@
+class DoWhileExample {
+    public static void main(String[] args) {
+        int i = 1;
+
+        do {
+            System.out.println("Do-While Loop: i = " + i);
+            i++;
+        } while(i <= 5);
+    }
+}
